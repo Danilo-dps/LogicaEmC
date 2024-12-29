47af7270c87cd738f79f1057fdc600aabb504554
Danilo Pereira da silva <danilo@marighella.danilo>
2024-12-29 11:18:48 -0300

    Corrigindo o nome

---
+++ arrays/pares-ordem-invertida.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define TAM 10
+
+int main() {
+
+    int a[TAM];
+
+    int i;
+    printf("Lê os valores do Array\n");
+    for (i = 0; i < TAM; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Imprime os pares, percorrendo em ordem invertida o array\n");
+    for (i = TAM - 1; i >= 0; i--){
+        
+        if(a[i] % 2 == 0){
+            printf("%d ", a[i]);
+        }
+    }
+
+    return 0;
+}
